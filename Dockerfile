0995e3431ecaef2daa8fded44f4480e15b43a532
emilie <emilie.poccioli@gmail.com>
2014-12-11 15:28:29 +0000

    Enabled SSL for web apps and console

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 # Use latest jboss/base-jdk:7 image as the base
 FROM jboss/base-jdk:7
-
 MAINTAINER Dell Cloud Market Place <Cloud_Marketplace@dell.com>
  
 # The base image specifies user "jboss". We need to switch temporarily.
 USER root
-RUN yum -y install pwgen 
-RUN usermod -s /bin/bash jboss
+RUN yum -y install pwgen \
+    && usermod -s /bin/bash jboss
 USER jboss
 
 # Set the WILDFLY_VERSION env variable
@@ -15,8 +14,11 @@ ENV WILDFLY_VERSION 8.2.0.Final
 # Add the WildFly distribution to /opt, and make wildfly the owner of the
 # extracted tar content.
 RUN cd $HOME && curl http://download.jboss.org/wildfly/$WILDFLY_VERSION/\
-wildfly-$WILDFLY_VERSION.tar.gz | tar zx && \
-mv $HOME/wildfly-$WILDFLY_VERSION $HOME/wildfly
+    wildfly-$WILDFLY_VERSION.tar.gz | tar zx && \
+    mv $HOME/wildfly-$WILDFLY_VERSION $HOME/wildfly
+
+# Add standalone.xml to enable SSL for console management and web applications
+ADD standalone.xml /opt/jboss/wildfly/standalone/configuration/standalone.xml
 
 # Set the JBOSS_HOME env variable
 ENV JBOSS_HOME /opt/jboss/wildfly
@@ -26,8 +28,8 @@ ADD run.sh /run.sh
 
 VOLUME ["/opt/jboss/wildfly/standalone/deployments"]
 
-# Expose the ports we're interested in
-EXPOSE 8080 9990
+# Expose Wildfly ports (HTTP/HTTPS, management console on HTTP and HTTPS)
+EXPOSE 8080 9990 8443 9993
 
 # Make it the default command to run on boot
 USER root
--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+CONFIG_DIR="/opt/jboss/wildfly/standalone/configuration"
+STANDALONE_XML="$CONFIG_DIR/standalone.xml"
+
 # Change ownership and permissions of the deployment directory
 DEPLOYMENT_SCANNER_PATH="/opt/jboss/wildfly/standalone/deployments"
 chown -R jboss:jboss $DEPLOYMENT_SCANNER_PATH 
@@ -19,6 +22,34 @@ echo
 echo "    user name: admin"
 echo "    password : $PASS"
 echo
+
+# Generate Self-Signed certificate for web-based applications
+UNDERTOW_SSL_CERT_PWD=`pwgen -c -n -1 12`
+keytool -genkey -noprompt \
+-alias web_ssl \
+-dname "CN=www.dell.com, OU=MarketPlace, O=Dell, C=US" \
+-keyalg RSA \
+-storepass $UNDERTOW_SSL_CERT_PWD \
+-keypass $UNDERTOW_SSL_CERT_PWD \
+-validity 360 \
+-keysize 2048 \
+-keystore $CONFIG_DIR/web.keystore
+
+# Generate Self-Signed certificate for management console
+CONSOLE_SSL_CERT_PWD=`pwgen -c -n -1 12`
+keytool -genkey -noprompt \
+-alias console_ssl \
+-dname "CN=www.dell.com, OU=MarketPlace, O=Dell, C=US" \
+-keyalg RSA \
+-storepass $CONSOLE_SSL_CERT_PWD \
+-keypass $CONSOLE_SSL_CERT_PWD \
+-validity 360 \
+-keysize 2048 \
+-keystore $CONFIG_DIR/console.keystore
+
+# Inject keystores passwords in standalone.xml
+sed -i 's/$CONSOLE_SSL_CERT_PWD/'$CONSOLE_SSL_CERT_PWD'/g' $STANDALONE_XML
+sed -i 's/$UNDERTOW_SSL_CERT_PWD/'$UNDERTOW_SSL_CERT_PWD'/g' $STANDALONE_XML
 
 # This will boot WildFly in the standalone mode and bind to all interface
 su jboss -c "/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0"
